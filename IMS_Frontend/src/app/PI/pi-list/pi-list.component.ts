36a79e30d68cabcc56da29308147fa3ea1adec5e
mozahercse <Nipo@01839555609>
2025-09-12 22:42:10 +0600

    Un-Approve To Full Approve Done..

--- IMS_Frontend/src/app/PI/pi-list/pi-list.component.ts
+++ IMS_Frontend/src/app/PI/pi-list/pi-list.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { ButtonModule } from 'primeng/button';
@@ -13,6 +13,7 @@ import Swal from 'sweetalert2';
 import { DividerModule } from 'primeng/divider';
 import { FieldsetModule } from 'primeng/fieldset';
 import { DropdownModule } from "primeng/dropdown";
+import { MasterEntryService } from 'src/app/services/masterEntry/masterEntry.service';
 
 @Component({
   standalone:true,
@@ -22,11 +23,12 @@ import { DropdownModule } from "primeng/dropdown";
   imports: [FormsModule,ReactiveFormsModule, CommonModule, TableModule, InputTextModule, DialogModule, ButtonModule, BsDatepickerModule, DividerModule, FieldsetModule, DropdownModule]
 })
 export class PiListComponent implements OnInit {
-@Input() PiStatus!:any;
+  @Input() PiStatus!:any;
+  datePipe = new DatePipe('en-US');
   DataTable!:any[];
   PIData!:any;
   PIDetails!:any[];
-  PITypeList:any[]=[{value:0,text:''},{value:1,text:'LC'},{value:1,text:'Cash'}];
+  PITypeList:any[]=[{value:1,text:'LC'},{value:2,text:'Cash'}];
   first: any=1;
   rows: any=10;
   totalRecords!: number;
@@ -39,10 +41,11 @@ export class PiListComponent implements OnInit {
   ShowbtnRej:boolean=false;
   ShowbtnSpecial:boolean=false;
 
-    SearchFormgroup!: FormGroup;
+  SearchFormgroup!: FormGroup;
+  selectedRows: any[] = []; 
 
 
-  constructor(
+  constructor( private service:MasterEntryService,
       private getDataService: GetDataService,
       private gs: GlobalServiceService,
       private title: Title,
@@ -62,7 +65,11 @@ export class PiListComponent implements OnInit {
       PageNumber: [''],
       PageSize : ['']
     });
-    }
+
+ 
+  }
+
+
 
   ngOnInit() {
 
@@ -142,24 +149,23 @@ OpenSpecialApprove(Id:number){
     this.SearchFormgroup.controls['PageSize'].setValue(this.rows);
     let permas=this.SearchFormgroup.value;
 
-    /*
-    {
-          PINO :'',
-          FromDate :'',
-          ToDate :'',
-          Shipper :'',
-          Consignee :'',
-          CreateBY :'',
-          PIType :null,
+    this.DataTable=[];
+
+    
+      const procedureData = {
+        procedureName: 'usp_ProformaInvoice_GetDataDataTable',
+        parameters: {
+          PINO :permas.PINO?permas.PINO:'',
+          FromDate :permas.FromDate? this.datePipe.transform(permas.FromDate, 'yyyy-MM-dd') :'',
+          ToDate :permas.ToDate? this.datePipe.transform(permas.ToDate, 'yyyy-MM-dd') :'',
+          Shipper :permas.Shipper?permas.Shipper:'',
+          Consignee :permas.Consignee?permas.Consignee:'',
+          CreateBY :permas.CreateBY?permas.CreateBY:'',
+          PIType :permas.PIType?permas.PIType:null,
           PI_Status   : this.PiStatus,
           PageNumber   : this.first,
           PageSize    : this.rows
         }
-          */
-    
-      const procedureData = {
-        procedureName: 'usp_ProformaInvoice_GetDataDataTable',
-        parameters: permas
       };
       console.clear();
       console.log(procedureData);
@@ -169,7 +175,10 @@ OpenSpecialApprove(Id:number){
         next: (results) => {
           if (results.status) {
             this.DataTable = JSON.parse(results.data).Tables1;
-            this.totalRecords=this.DataTable[0].TotalCount;
+            this.totalRecords=this.DataTable[0]?.TotalCount;
+
+      
+
           } else if (results.msg == 'Invalid Token') {
             Swal.fire('Session Expired!', 'Please Login Again.', 'info');
             this.gs.Logout();
@@ -179,4 +188,43 @@ OpenSpecialApprove(Id:number){
       });
     }
 
+
+    getSelectedRows(status:string) {
+      if (this.selectedRows.length<=0) {
+        return;
+      }
+      const selectedIds = this.selectedRows.map(r => r.PI_Master_ID);
+
+      Swal.fire({
+        title: `Do you want to ${status}?`,
+        showDenyButton: true,
+        confirmButtonText: "Yes",
+      }).then((result) => {
+
+        if (result.isConfirmed) {
+
+          selectedIds.forEach((id:any)=>{
+            let queryParams={Status:status,LastUpdateDate:new Date()};
+            let condition={PI_Master_ID:id};
+            
+            this.service.UpdateData(queryParams,condition,'tbl_pi_master').subscribe({
+              next: (results) => {
+
+                if (results.status) {
+
+                  Swal.fire(results.messageType, results.message, 'success').then(()=>{
+                    this.ngOnInit();
+                  });
+                } 
+                else if (results.message == 'Invalid Token') {
+                  Swal.fire('Session Expired!', 'Please Login Again.', 'info');
+                  this.gs.Logout();
+                }
+              },
+              error: (err) => { },
+            });
+          });
+        }
+      });
+    }
 }
