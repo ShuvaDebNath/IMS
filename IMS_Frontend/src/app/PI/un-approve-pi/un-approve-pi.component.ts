bc21a22bf04c868438478abbd95117fa579cd46b
Shuva Deb Nath <shuvadebnathbd@gmail.com>
2025-10-20 16:41:52 +0600

    initial commit

--- IMS_Frontend/src/app/PI/un-approve-pi/un-approve-pi.component.ts
+++ IMS_Frontend/src/app/PI/un-approve-pi/un-approve-pi.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-//import { PiListComponent } from '../pi-list/pi-list.component';
+// import { PiListComponent } from '../pi-list/pi-list.component';
 
 @Component({
   standalone:true,
   selector: 'app-un-approve-pi',
   templateUrl: './un-approve-pi.component.html',
   styleUrls: ['./un-approve-pi.component.css'],
-  //imports: [PiListComponent]
+  // imports: [PiListComponent]
 })
 export class UnApprovePiComponent implements OnInit {
   constructor(){}
