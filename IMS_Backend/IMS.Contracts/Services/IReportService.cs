a3df8bf41cb6fb3cf9bc51f0128e5908810dab9c
ADNath <dawaking8@gmail.com>
2025-12-02 18:22:26 +0600

    Customer report on going

--- IMS_Backend/IMS.Contracts/Services/IReportService.cs
+++ IMS_Backend/IMS.Contracts/Services/IReportService.cs
@@ -8,6 +8,7 @@ namespace Boilerplate.Contracts.Services;
 public interface IReportService
 {
     public Task<DataSet> SampleRequestReport(string fromDate, string toDate, string requestStatus,string UserID);
+    public Task<DataSet> CustomerReport(string Superior_Id, string Customer_Id, string Status, string SentBy);
     public Task<DataSet> ProformaInvoiceReport(int PI_Master_ID);
     public Task<DataSet> CommercialInvoiceReports(string commercialInvoiceNo, string reportType);
     public Task<DataSet> DeliveryChallanReport(string challanNo);
