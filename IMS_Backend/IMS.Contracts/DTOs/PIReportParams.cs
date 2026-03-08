f58f35c370be5bd91d756b98b49156692b861ca5
Amit Deb Nath <dawaking8@gmail.com>
2026-03-08 19:44:08 +0600

    init

---
+++ IMS_Backend/IMS.Contracts/DTOs/PIReportParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingBackEnd.DAL.DTOs
+{
+    public class PIReportParams
+    {
+        public string RptType { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string? PI_Master_Id { get; set; }
+        public string? ClientId { get; set; }
+        public string? User_Id { get; set; }
+        public int PageLength { get; set; }
+        public int PageNo { get; set; }
+        public string? SearchParam { get; set; }
+    }
+}
